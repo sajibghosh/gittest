de162f27c004602cc6119971e90fe91fed57c360
sajibghosh <sajibghosh36@gmail.com>
2018-09-17 01:21:31 +0600

    file modified to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
 public class hello{
     public static void main(string[] args) {
         system.out.println("vs code is awsome..");
+        gretting("sajib ghosh");
+    }
+
+    public static void display(string name){
+        system.out.println("welcome " + name);
     }
 }
